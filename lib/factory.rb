4278cb7be580a24f86b78b8f6b99dee07aed40be
Emma Yeroshek <emma.yeroshek@gmail.com>
2018-11-15 18:10:30 +0200

    Fixed ArgumentError,method values_at

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,105 +1,91 @@
-# * Here you must define your `Factory` class.
-# * Each instance of Factory could be stored into variable. The name of this variable is the name of created Class
-# * Arguments of creatable Factory instance are fields/attributes of created class
-# * The ability to add some methods to this class must be provided while creating a Factory
-# * We must have an ability to get/set the value of attribute like [0], ['attribute_name'], [:attribute_name]
-#
-# * Instance of creatable Factory class should correctly respond to main methods of Struct
-# - each
-# - each_pair
-# - dig
-# - size/length
-# - members
-# - select
-# - to_a
-# - values_at
-# - ==, eql?
 require 'pry'
 
 class Factory
- def self.new(*args, &block)
-    raise ArgumentError, "Wrong number of arguments (0 for 1+)" if args.length < 1
+  class << self
+    def new(*args, &block)
+      Class.new do
+        attr_accessor(*args)
 
-    Class.new do
-      attr_accessor(*args)
-      class_eval(&block) if block_given?
+        define_method(:initialize) do |*arg|
+          raise ArgumentError, 'Extra args passed then needs' unless args.count == arg.count
 
-      define_method(:initialize) do |*arg|
-        args.each_index do |i|
-          unless args[i].is_a? Symbol
-            raise NameError, "Identifier #{args[i]} needs to be constant"
-          end
-          instance_variable_set("@#{args[i]}", arg[i])
-        end
-      end
-
-      define_method(:[]=) do |arg, value|
-        if (arg.is_a? String) || (arg.is_a? Symbol)
-          instance_variable_set("@#{arg}", value)
-        elsif arg.is_a? Integer
-          instance_variable_set(instance_variables[arg], value)
-        end
-      end
-
-      define_method(:[]) do |arg|
-        if (arg.is_a? String) || (arg.is_a? Symbol)
-          instance_variable_get("@#{arg}")
-        elsif arg.is_a? Integer
-          instance_variable_get(instance_variables[arg])
-        end
-      end
-
-      define_method(:members) do
-        instance_variables.map { |instance_var| instance_var.to_s.delete('@').to_sym}
-      end
-
-      define_method(:each) do |&block|
-        block ? members.each { |attribute| block.call(send(attribute)) } : enum_for(:each)
-      end
-
-      define_method(:each_pair) do |&block|
-        block ? to_h.each_pair(&block) : enum_for(:each)
-      end
-
-      define_method(:length) do
-        self.instance_variables.size
-      end
-
-      define_method(:select) do |&block|
-        block ? values.select(&block) : enum_for(:select)
-      end
-
-      define_method(:values) do
-        instance_variables.map { |var| instance_variable_get(var) }
-      end
-
-      define_method(:values_at) do |key, value|
-        values_at(key, value)
-      end
-      
-      define_method(:eql?) do |obj|
-        self.class == obj.class && (self.values.eql? obj.values)
-      end
-
-      define_method(:to_h) do
-        res = {}
-        b = instance_variables.map {|var| hash = {var => instance_variable_get(var)}}
-        b.each do |val|
-          val.each do |key, value|
-            key = key.to_s.delete('@').to_sym
-            res[key] = Hash[key, value]
+          args.each_index do |i|
+            unless args[i].is_a? Symbol
+              raise NameError, "Identifier #{args[i]} needs to be constant"
+            end
+            instance_variable_set("@#{args[i]}", arg[i])
           end
         end
-        res
-      end
 
-      define_method(:dig) do |*args|
-          to_h.dig(*args)
-      end
+        define_method(:[]=) do |arg, value|
+          if (arg.is_a? String) || (arg.is_a? Symbol)
+            instance_variable_set("@#{arg}", value)
+          elsif arg.is_a? Integer
+            instance_variable_set(instance_variables[arg], value)
+          end
+        end
 
-      alias :== :eql?
-      alias :to_a :values
-      alias :size :length
+        define_method(:[]) do |arg|
+          if (arg.is_a? String) || (arg.is_a? Symbol)
+            instance_variable_get("@#{arg}")
+          elsif arg.is_a? Integer
+            instance_variable_get(instance_variables[arg])
+          end
+        end
+
+        define_method(:members) do
+          instance_variables.map { |var| var.to_s.delete('@').to_sym}
+        end
+
+        define_method(:each) do |&block|
+          members.each { |attr| block.call(send(attr)) }
+        end
+
+        define_method(:each_pair) do |&block|
+          to_h.each_pair(&block)
+        end
+
+        define_method(:length) do
+          self.instance_variables.size
+        end
+
+        define_method(:select) do |&block|
+          values.select(&block)
+        end
+
+        define_method(:values) do
+          instance_variables.map { |var| instance_variable_get(var) }
+        end
+
+        define_method(:values_at) do |*index|
+          instance_variables.values_at(*index).map { |values| instance_variable_get values }
+        end
+
+        define_method(:eql?) do |obj|
+          self.class == obj.class && (self.values.eql? obj.values)
+        end
+
+        define_method(:to_h) do
+          res = {}
+          b = instance_variables.map {|var| hash = {var => instance_variable_get(var)}}
+          b.each do |val|
+            val.each do |key, value|
+              key = key.to_s.delete('@').to_sym
+              res[key] = Hash[key, value]
+            end
+          end
+          res
+        end
+
+        define_method(:dig) do |*args|
+           to_h.dig(*args)
+        end
+
+        class_eval(&block) if block_given?
+        alias :== :eql?
+        alias :to_a :values
+        alias :size :length
+      end
     end
   end
 end
