0117e71859bd9c75c32166d30d8d7f4686558296
Emma Yeroshek <emma.yeroshek@gmail.com>
2018-11-16 20:27:01 +0200

    Bugfix after review

--- lib/factory.rb
+++ lib/factory.rb
@@ -3,34 +3,30 @@ require 'pry'
 class Factory
   class << self
     def new(*args, &block)
+      const_set(args.shift.capitalize, new(*args, &block)) if args.first.is_a? String
+      build_class(*args, &block)
+    end
+
+    def build_class(*args, &block)
       Class.new do
         attr_accessor(*args)
 
         define_method(:initialize) do |*arg|
           raise ArgumentError, 'Extra args passed then needs' unless args.count == arg.count
-
+          raise NameError, 'Identifier needs to be constant' if args.reject {|arg| arg.is_a? Symbol}.any?
           args.each_index do |i|
-            unless args[i].is_a? Symbol
-              raise NameError, "Identifier #{args[i]} needs to be constant"
-            end
             instance_variable_set("@#{args[i]}", arg[i])
           end
         end
 
         define_method(:[]=) do |arg, value|
-          if (arg.is_a? String) || (arg.is_a? Symbol)
-            instance_variable_set("@#{arg}", value)
-          elsif arg.is_a? Integer
-            instance_variable_set(instance_variables[arg], value)
-          end
+          return instance_variable_set("@#{arg}", value) if (arg.is_a? String) || (arg.is_a? Symbol)
+          instance_variable_set(instance_variables[arg], value) if arg.is_a? Integer
         end
 
         define_method(:[]) do |arg|
-          if (arg.is_a? String) || (arg.is_a? Symbol)
-            instance_variable_get("@#{arg}")
-          elsif arg.is_a? Integer
-            instance_variable_get(instance_variables[arg])
-          end
+          return instance_variable_get("@#{arg}") if (arg.is_a? String) || (arg.is_a? Symbol)
+          instance_variable_get(instance_variables[arg]) if arg.is_a? Integer
         end
 
         define_method(:members) do
@@ -38,7 +34,7 @@ class Factory
         end
 
         define_method(:each) do |&block|
-          members.each { |attr| block.call(send(attr)) }
+          members.each { |attr| block.call(public_send(attr)) }
         end
 
         define_method(:each_pair) do |&block|
@@ -46,7 +42,7 @@ class Factory
         end
 
         define_method(:length) do
-          self.instance_variables.size
+          instance_variables.size
         end
 
         define_method(:select) do |&block|
@@ -62,24 +58,24 @@ class Factory
         end
 
         define_method(:eql?) do |obj|
-          self.class == obj.class && (self.values.eql? obj.values)
+          self.class == obj.class && (values.eql? obj.values)
         end
 
         define_method(:to_h) do
-          res = {}
-          b = instance_variables.map {|var| hash = {var => instance_variable_get(var)}}
-          b.each do |val|
-            val.each do |key, value|
-              key = key.to_s.delete('@').to_sym
-              res[key] = Hash[key, value]
-            end
-          end
-          res
+          Hash[members.zip(values)]
         end
 
         define_method(:dig) do |*args|
-           to_h.dig(*args)
+          args.inject(self) {|h,a| h.fetch(a) }
         end
+
+        def fetch(name)
+          __send__(name)
+        end
+
+        #define_method(:dig) do |*args|
+         # to_h.dig(*args)
+        #end
 
         class_eval(&block) if block_given?
         alias :== :eql?
